8a724f62bae733f33a39bb42eb627f042fb28fe8
nhatlan <vanphamnhatlan93@gmail.com>
2016-02-13 16:28:48 +0700

    solution 01 run

--- relationship.js
+++ relationship.js
@@ -1,9 +1,22 @@
 function getRelationship(x, y) {
     // Your code goes here!
-    if(x<y){
-        var answer = x + " is smaller than " + y; 
+    var answer; 
+    if (isNaN(x) && isNaN(y)){
+    	answer = "Can't compare relationships because " + x + " and "+ y+  " are not numbers";
+    } else if (isNaN(x) || isNaN(y)){
+    	if (isNaN(x)){
+    		answer = "Can't compare relationships because " + x + " is not a number";
+    	} else if (isNaN(y)){
+    		answer = "Can't compare relationships because " + y + " is not a number";
+    	}
+    } else if(x<y){
+    	answer =x + " is smaller than " + y;
+    } else if (x>y){
+    	answer =x + " is greater than " + y;
+    } else if (x===y){
+    	answer =x + " is equal " + y;
     }
-    return answer;
+    return answer; 
 }
 console.log(getRelationship(1,4));
 console.log(getRelationship(1,1));
@@ -13,3 +26,5 @@ console.log(getRelationship(3));
 console.log(getRelationship("hi"));
 console.log(getRelationship(NaN));
 console.log(getRelationship(NaN, undefined));
+
+console.log(false ? "true!" : "false.");
